456cc35bf5bc1a8810c89c0b290915dd2cd20fe2
arman ahmed jony <armanahmedjony786@gmail.com>
2022-04-16 02:01:48 +0600

    added website into cms

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import ContentIndex from './modules/content';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ContentIndex from "./modules/content";
+import Website from "./modules/website";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="m-4">
-      <h1 className="text-3xl text-center font-bold">Sinto Shinu Content Manager</h1>
-      <ContentIndex />
-    </div>
+    <Routes>
+      <Route path="/" element={<Website />} />
+      <Route
+        path="/cms"
+        element={
+          <div className="m-4">
+            <h1 className="text-3xl text-center font-bold">
+              Sinto Shinu Content Manager
+            </h1>
+            <ContentIndex />
+          </div>
+        }
+      />
+      } />
+    </Routes>
   );
 }
 
