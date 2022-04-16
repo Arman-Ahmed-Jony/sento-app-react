744a277135f7b5e1634884a3df8588c24883a00d
arman ahmed jony <armanahmedjony786@gmail.com>
2022-04-16 13:04:17 +0600

    added content form

---
+++ src/modules/content/components/ContentForm.jsx
@@ -0,0 +1,29 @@
+import { TextField } from "@mui/material";
+const ContentForm = ({ title, content, onTitleChange, onContentChange }) => {
+  return (
+    <>
+      <div>
+        <TextField
+          required
+          label="key"
+          onChange={onTitleChange}
+          value={title}
+          fullWidth
+        />
+      </div>
+      <div className="mt-4">
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Value"
+          multiline
+          maxRows={4}
+          fullWidth
+          onChange={onContentChange}
+          value={content}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ContentForm;
