f1c4fdeac59e1b9e403872669dd8606a0524c787
arman ahmed jony <armanahmedjony786@gmail.com>
2022-04-16 12:59:32 +0600

    added new ui of cms

--- src/modules/content/components/Content.jsx
+++ src/modules/content/components/Content.jsx
@@ -1,23 +1,54 @@
-import React from "react";
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import { TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import EditIcon from "@mui/icons-material/Edit";
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
 
 function Content({ keyName, value, onChange }) {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
   return (
     <>
-      <div className="flex flex-row my-4">
-        <div className="basis-1/4">{keyName}</div>
-        <div className="basis-3/4">
-        <TextField
-          required
-          id="outlined-required"
-          label={keyName}
-          multiline
-          rows={4}
-          fullWidth
-          defaultValue={value}
-        />
-        </div>
-      </div>
+      <Grid item xs={12} md={6}>
+        <Demo>
+          <List dense={dense}>
+            <ListItem
+              secondaryAction={
+                <IconButton edge="end" aria-label="edit">
+                  <EditIcon />
+                </IconButton>
+              }
+            >
+              <ListItemText
+                secondary={
+                  <div className="flex flex-row my-4">
+                    <div className="basis-2/12">{keyName}</div>
+                    <div className="basis-10/12">
+                      <TextField
+                        required
+                        id="outlined-required"
+                        label={keyName}
+                        multiline
+                        rows={4}
+                        fullWidth
+                        defaultValue={value}
+                      />
+                    </div>
+                  </div>
+                }
+              />
+            </ListItem>
+          </List>
+        </Demo>
+      </Grid>
     </>
   );
 }
